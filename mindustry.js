bdb207d3944229b849e957a11732439b1de80938
squi2rel <501410836@qq.com>
2023-04-30 09:54:01 +0800

    fix

--- mindustry.js
+++ mindustry.js
@@ -1014,10 +1014,10 @@ class StreamBuilder{
     type;
     total;
     stream;
-    #length;
+    length;
     #buf;
     constructor(packet){
-        this.#length=0;
+        this.length=0;
         this.id=packet.id;
         this.type=packet.type;
         this.total=packet.total;
@@ -1025,11 +1025,11 @@ class StreamBuilder{
     }
     add(data){
         if(!data instanceof Buffer) throw new TypeError("data must be a buffer.")
-        this.#length+=data.length;
+        this.length+=data.length;
         this.#buf.push(data)
     }
     isDone(){
-        return this.#length>=this.total
+        return this.length>=this.total
     }
     build(){
         let s=new (Packets.get(this.type))();
