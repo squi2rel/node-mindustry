57cafb220c831e359637ca2e3d8782cb866dbddf
squi2rel <501410836@qq.com>
2023-10-20 22:18:12 +0800

    fix

--- mindustry.js
+++ mindustry.js
@@ -1,6 +1,7 @@
 const { EventEmitter } = require("events");
 const Packets = require("./lib/Packets");
 const NetClient = require("./lib/NetClient");
+const DataStream = require("./lib/DataStream");
 const World = require("./lib/World");
 const dgram = require("dgram");
 
